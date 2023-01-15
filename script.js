e7103a17474b8a0a4fbe859ff8fa69c1dc838ab1
Liam Alexander <liamalexander7@hotmail.co.uk>
2023-01-15 20:57:10 +0000

    start js

--- script.js
+++ script.js
@@ -1,2 +1,62 @@
 "use strict";
 
+const billInput = document.querySelector(".bill-input");
+const tipBtns = document.querySelectorAll(".tip-btn");
+const customTipInput = document.querySelector(".custom");
+const peopleInput = document.querySelector(".people-input");
+const resetBtn = document.querySelector(".reset-btn");
+const totalTip = document.querySelector(".total-tip");
+const totalPay = document.querySelector(".total-pay");
+const btnContainer = document.querySelector(".btn-container");
+
+let selectedTip;
+let finalBill;
+let totalPeople;
+
+billInput.addEventListener("keyup", function () {
+  const bill = billInput.value;
+  console.log(bill);
+  finalBill = bill;
+  // return finalBill;
+});
+
+btnContainer.addEventListener("click", function(e) {
+  const target = e.target.dataset.tipValue;
+  selectedTip = target;
+  console.log(target);
+  // return selectedTip;
+  
+  tipBtns.forEach(btn => {
+    // btn.classList.remove(".target-btn");
+    if (btn.dataset.tipValue === "") {
+      return;
+    }
+    if (btn.dataset.tipValue === target) {
+      // e.target.closest(".tip-btn").style.backgroundColor = "blue";
+      btn.style.backgroundColor = "blue";
+      // btn.classList.add(".target-btn");
+    } else {
+      btn.style.backgroundColor = "var(--color-btns)";
+      // btn.classList.remove(".target-btn");
+    }
+  })
+});
+
+peopleInput.addEventListener("keyup", function () {
+  const people = peopleInput.value;
+  console.log(people);
+  totalPeople = people;
+  // return totalPeople;
+});
+
+// console.log(finalBill, selectedTip);
+
+// tipBtns.forEach(btn => {
+//   btn.addEventListener("click", function() {
+//     btn.style.backgroundColor = "var(--color-btns)";
+//     btn.style.backgroundColor = "blue";
+//     btn.classList.add(".clicked");
+//     console.log(btn.dataset);
+//   })
+// })
+
