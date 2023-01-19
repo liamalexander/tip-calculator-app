e8ee993cbd4077e2192675d0402faa95d939fc11
Liam Alexander <liamalexander7@hotmail.co.uk>
2023-01-19 12:48:38 +0000

    small js change

--- script.js
+++ script.js
@@ -25,11 +25,10 @@ eventTypes.forEach(type => {
     let billTotalPerPerson;
 
     // if (e.target.className !== "people-input" || "bill-input" || "tip-btn") return;
-    console.log(e.target.className);
 
     if (type == "click" && e.target.className == "tip-btn") {
       selectedTip = +e.target.dataset.tipValue;
-      console.log(selectedTip, typeof(selectedTip));
+
       tipBtns.forEach(btn => {
         +btn.dataset.tipValue === selectedTip ? btn.classList.add("target-btn") 
         : btn.classList.remove("target-btn"); 
@@ -38,17 +37,16 @@ eventTypes.forEach(type => {
 
     if (type == "keyup" && e.target.className == "bill-input") {
       finalBill = +e.target.value;
-      console.log(finalBill, typeof(finalBill));
     };
 
     if (type == "keyup" && e.target.className == "people-input") {
       totalPeople = +e.target.value;
-      console.log(totalPeople, typeof(totalPeople));
     };
 
     if (type == "keyup" && e.target.id == "custom") {
-      selectedTip = +e.target.value;
-      console.log(selectedTip, typeof(selectedTip));
+      let customInput = e.target.value;
+      let customTip = customInput.replace("%", "");
+      selectedTip = +customTip;
       customTipInput.classList.add("target-btn");
 
       tipBtns.forEach(btn => {
@@ -59,18 +57,11 @@ eventTypes.forEach(type => {
 
     if (selectedTip && totalPeople && finalBill) {
       liveTip = (finalBill / 100) * selectedTip;
-      // console.log(liveTip);
       liveTotalBill = finalBill + liveTip;
-      // console.log(liveTotalBill);
       tipTotalPerPerson = liveTip / totalPeople;
-      // console.log(tipTotalPerPerson);
       billTotalPerPerson = liveTotalBill / totalPeople;
-      // console.log(billTotalPerPerson);
       totalTip.textContent = `$ ${tipTotalPerPerson.toFixed(2)}`;
       totalPay.textContent = `$ ${billTotalPerPerson.toFixed(2)}`;
-      // tipTotalPerPerson = 0;
-      // billTotalPerPerson = 0;
-      //  ABOVE TO TRY CLEAR THE TOTALS AFTER RESET AND NEXT CLICK
     }
 
     if (type == "click" && e.target.className == "reset-btn") {
@@ -95,76 +86,3 @@ eventTypes.forEach(type => {
     }
   });
 });
-
-
-
-// billInput.addEventListener("keyup", function () {
-//   const bill = billInput.value;
-//   console.log(bill);
-//   finalBill = bill;
-//   // return finalBill;
-// });
-
-// btnContainer.addEventListener("click", function(e) {
-//   const target = e.target.dataset.tipValue;
-//   selectedTip = target;
-//   console.log(target);
-//   // return selectedTip;
-  
-//   tipBtns.forEach(btn => {
-//     // btn.classList.remove(".target-btn");
-//     if (btn.dataset.tipValue === "") {
-//       return;
-//     }
-//     if (btn.dataset.tipValue === target) {
-//       // e.target.closest(".tip-btn").style.backgroundColor = "blue";
-//       btn.style.backgroundColor = "blue";
-//       // btn.classList.add(".target-btn");
-//     } else {
-//       btn.style.backgroundColor = "var(--color-btns)";
-//       // btn.classList.remove(".target-btn");
-//     }
-//   })
-// });
-
-//  TRY THIS CODE ON WEBPAGE OPEN (STACK OVERFLOW)
-
-// document.addEventListener("keyup", function (e) {
-//   if(e.target.className == "bill-input") {
-//     finalBill = billInput.value;
-//     console.log(finalBill);
-
-//   }
-//   if(e.target.className == "people-input") {
-//     totalPeople = peopleInput.value;
-//     console.log(totalPeople);
-//   }
-// })
-
-
-//  OR
-
-// [ peopleInput, billInput ].forEach(function(element) {
-//   element.addEventListener("input", function() {
-//      this function does stuff
-//   });
-// });
-
-// peopleInput.addEventListener("keyup", function () {
-//   const people = peopleInput.value;
-//   console.log(people);
-//   totalPeople = people;
-//   // return totalPeople;
-// });
-
-// console.log(finalBill, selectedTip);
-
-// tipBtns.forEach(btn => {
-//   btn.addEventListener("click", function() {
-//     btn.style.backgroundColor = "var(--color-btns)";
-//     btn.style.backgroundColor = "blue";
-//     btn.classList.add(".clicked");
-//     console.log(btn.dataset);
-//   })
-// })
-
